a711eb7840ee02fbb994984db01f20594e198852
Sean Stavropoulos <sean.stavro@gmail.com>
2018-11-18 14:59:10 -0800

    Backport/Merge pull request #103 from dbernheisel/db-ecto-3
    Fix Ecto 3.0 warnings
     Conflicts:
    	mix.exs
    	test/schema_test.exs

--- mix.exs
+++ mix.exs
@@ -4,14 +4,16 @@ defmodule Arc.Ecto.Mixfile do
   @version "0.8.0"
 
   def project do
-    [app: :arc_ecto,
-     version: @version,
-     elixir: "~> 1.0",
-     deps: deps(),
+    [
+      app: :arc_ecto,
+      version: @version,
+      elixir: "~> 1.0",
+      deps: deps(),
 
-    # Hex
-     description: description(),
-     package: package()]
+      # Hex
+      description: description(),
+      package: package()
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,10 +30,12 @@ defmodule Arc.Ecto.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Sean Stavropoulos"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/stavro/arc_ecto"},
-     files: ~w(mix.exs README.md lib)]
+    [
+      maintainers: ["Sean Stavropoulos"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/stavro/arc_ecto"},
+      files: ~w(mix.exs README.md lib)
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -45,8 +49,8 @@ defmodule Arc.Ecto.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:arc,  "~> 0.8.0"},
-      {:ecto, "~> 2.1"},
+      {:arc, "~> 0.8.0"},
+      {:ecto, "~> 2.1 or ~> 3.0"},
       {:mock, "~> 0.1.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
